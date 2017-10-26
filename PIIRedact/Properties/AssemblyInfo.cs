8d06a7463bdc240be2a8c4fc9eed4188d7ecd0e5
Musfiq <musfiq@users.noreply.github.com>
2017-10-25 23:08:29 -0700

    Nuspec update
    Updated description and project url in nuspec.

--- PIIRedact/Properties/AssemblyInfo.cs
+++ PIIRedact/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PII Redactor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Redacts PII information.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PII Redactor")]
